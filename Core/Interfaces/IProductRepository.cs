705973ca76a9ff9baf861c05a42d9a7b31a0781c
Teja Sai Srinivas Khajjayam <kajjayamsrinivas@gmail.com>
2024-02-18 20:41:04 -0600

    End of Section 3

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+using Core.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+       Task<Product> GetProductByIdAsync(int id);
+
+       Task<IReadOnlyList<Product>>  GetProductsAsync();
+
+       Task<IReadOnlyList<ProductBrand>>  GetProductBrandsAsync();
+
+       Task<IReadOnlyList<ProductType>>  GetProductTypesAsync();
+
+
+    }
+} 
